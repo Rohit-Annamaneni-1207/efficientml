6e68c6056255fad90d9e82e1ad4cb9c1fea0281b
Annamaneni Rohit <annamaneni12rohit@gmail.com>
2026-06-27 18:45:48 +0530

    core training loop

--- core/trainer.py
+++ core/trainer.py
@@ -1,4 +1,6 @@
 from typing import Dict, List, Optional
+import torch
+from tqdm import tqdm
 
 class Trainer:
     def __init__(self, model, optimizer, loss_fn, device, train_loader, val_loader=None):
@@ -12,16 +14,68 @@ class Trainer:
 
     def train_epoch(self) -> float:
         self.model.train()
-        loss = 0
-        # next_batch = next(iter(self.train_loader), None)
-        return loss
+        total_loss = 0
+        
+        for batch in self.train_loader:
+            # move data to device
+            batch = {k: v.to(self.device) for k, v in batch.items()}
+
+            # Extract labels and not let model compute loss
+            labels = batch.pop("labels")
+
+            # Clear the gradients
+            self.optimizer.zero_grad()
+
+            #model outputs
+            outputs = self.model(**batch)
+
+            # Calculate loss
+            loss = self.loss_fn(output.logits, labels)
+
+            # Backward pass
+            loss.backward()
+
+            # Update params
+            self.optimizer.step()
+
+            total_loss += loss.item()
+
+        avg_loss = total_loss/len(self.train_loader)
+
+        return avg_loss
 
     def validate(self) -> Optional[float]:
         self.model.eval()
-        val_loss = 0
-        # next_batch = next(iter(self.val_loader), None)
-        return val_loss
+        total_loss = 0
+
+        if self.val_loader is None:
+            return None
+
+        with torch.no_grad():
+            for batch in self.val_loader:
+
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+
+                labels = batch.pop("labels")
+
+                outputs = self.model(**batch)
+
+                loss = self.loss_fn(outputs.logits, labels)
+
+                total_loss += loss.item()
+
+        avg_loss = total_loss/len(self.val_loader)
+        return avg_loss
     
 
     def fit(self, epochs: int) -> Dict[str, List[float]]:
-        pass
+        history = {"train_loss":[], "val_loss": []}
+
+        for epoch in tqdm(range(epochs)):
+            train_loss = self.train_epoch()
+            val_loss = self.validate()
+
+            history["train_loss"].append(train_loss)
+            history["val_loss"].append(val_loss)
+
+        return history
